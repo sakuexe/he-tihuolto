b7cd13e2fa37d9edb7d09a7ed1c643a50562858a
sakuexe <saku.karttunen@gmail.com>
2023-08-07 11:34:04 +0300

    added texts and icons

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,9 +15,10 @@ export default function Card({
   const isOdd = index % 2 !== 0
 
   return (
-    <div className="bg-primary max-w-xs mx-auto drop-shadow-lg rounded-lg">
+    <div className="bg-primary max-w-xs mx-auto drop-shadow-lg rounded-lg group">
       <div
         className={`flex justify-between items-center h-12 px-5 text-xl font-bold rounded-lg
+        overflow-hidden
         ${isOdd ? 'bg-primary-200' : 'bg-secondary'}`}
       >
         <h3>{service.title}</h3>
@@ -26,7 +27,8 @@ export default function Card({
           alt=""
           role="presentation"
           draggable="false"
-          className="h-full py-1 aspect-square opacity-75"
+          className="h-full py-1 aspect-square opacity-75 transition-all 
+          scale-125 rotate-3 group-hover:scale-150"
         />
       </div>
       <p className="p-5 pt-3">{service.description}</p>

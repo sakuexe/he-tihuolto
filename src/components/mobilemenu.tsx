525110ce3c7ed350b25c7ecdfe1cc41cf6be4f5c
sakuexe <saku.karttunen@gmail.com>
2023-08-17 10:47:17 +0300

    small tweak to mobile menu height

--- src/components/mobilemenu.tsx
+++ src/components/mobilemenu.tsx
@@ -48,12 +48,12 @@ export default function MobileMenu(props: MenuProps) {
     <section
       ref={MenuRef}
       id="mobile-menu"
-      className={`fixed flex top-0 right-0 w-full max-w-md h-[100svh]
+      className={`fixed flex top-0 right-0 w-full max-w-md h-screen
           rounded-l-2xl drop-shadow-xl z-30 transition-all lg:hidden
           ${props.isVisible ? 'translate-x-0' : 'translate-x-full'}
           ${props.isPrimaryColor ? 'bg-primary' : 'bg-secondary'} `}
     >
-      <div className="mt-16 mb-8 mx-5 flex flex-col justify-between flex-grow semitall:mt-28">
+      <div className="mt-16 mb-12 mx-5 flex flex-col justify-between flex-grow semitall:mt-28">
         <ul className="text-center space-y-4 tall:space-y-8">
           <li className="space-y-2">
             <a
